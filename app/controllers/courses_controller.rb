a1a67b1a1f72bfc8e12393752f8fe4c5c13b315d
Arjun Venkataswamy <avenkat2@gmail.com>
2013-07-26 14:08:10 -0500

    adds a student list to the ctd view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,7 +6,8 @@ class CoursesController < ApplicationController
     url = "http://portal.starterleague.com/courses/47.json"
     api_response = open(url).read
     @ruby_response = JSON.parse(api_response)
-
+    @students = @ruby_response["students"]
+    # use @ruby_response to create an array of students called 'students'
     render 'ctd'
   end
 end
